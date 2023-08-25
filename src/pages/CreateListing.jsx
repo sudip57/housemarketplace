e77a044b952022e64ba3e6cbcfb7c0f2aa8f3955
sudip57 <paudelsudip730@gmail.com>
2023-08-25 22:44:34 +0530

    moved api key to .env

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,27 +1,30 @@
-import { useState, useEffect, useRef } from 'react';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useState, useEffect, useRef } from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import {
   getStorage,
   ref,
   uploadBytesResumable,
   getDownloadURL,
-} from 'firebase/storage';
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
-import { db } from '../firebase.config';
+} from 'firebase/storage'
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase.config'
 
-import { useNavigate } from 'react-router-dom';
-import Spinner from '../components/Spinner';
-import '../index.css';
-import { toast } from 'react-toastify';
-import axios from 'axios';
+import { useNavigate } from 'react-router-dom'
+import Spinner from '../components/Spinner'
+import '../index.css'
+import { toast } from 'react-toastify'
+import axios from 'axios'
 
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'
 
 // const api_key = import.meta.env.VITE_API_KEY;
 
+const apiKey = import.meta.env.VITE_API_KEY
+console.log(apiKey)
+
 const CreateListing = () => {
-  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
-  const [loading, setLoading] = useState(false);
+  const [geolocationEnabled, setGeolocationEnabled] = useState(true)
+  const [loading, setLoading] = useState(false)
   const [formData, setFormData] = useState({
     type: 'rent',
     name: '',
@@ -35,7 +38,7 @@ const CreateListing = () => {
     images: {},
     latitude: 0,
     longitude: 0,
-  });
+  })
 
   const {
     type,
@@ -51,119 +54,120 @@ const CreateListing = () => {
     images,
     latitude,
     longitude,
-  } = formData;
+  } = formData
 
   const onSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    setLoading(true);
+    setLoading(true)
 
     if (discountedPrice >= regularPrice) {
-      setLoading(false);
-      toast.error('Discounted Price must be less than regular price');
-      return;
+      setLoading(false)
+      toast.error('Discounted Price must be less than regular price')
+      return
     }
 
     if (images.length > 6) {
-      setLoading(false);
-      toast.error('Max 6 images ');
-      return;
+      setLoading(false)
+      toast.error('Max 6 images ')
+      return
     }
 
-    let geolocation = {};
-    let location;
-
+    let geolocation = {}
+    let location
     if (geolocationEnabled) {
       const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyCSvd-9KpasLlxOaXhtv3dvGYvU3_ytYRk`
-      );
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${
+          import.meta.env.VITE_API_KEY
+        }`,
+      )
 
-      const data = await response.json();
+      const data = await response.json()
 
-      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
-      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
+      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
 
       location =
         data.status === 'ZERO_RESULTS'
           ? undefined
-          : data.results[0]?.formatted_address;
+          : data.results[0]?.formatted_address
 
       if (location === undefined || location.includes('undefined')) {
-        setLoading(false);
-        toast.error('Please enter a correct address');
-        return;
+        setLoading(false)
+        toast.error('Please enter a correct address')
+        return
       }
     } else {
-      geolocation.lat = latitude;
-      geolocation.lng = longitude;
+      geolocation.lat = latitude
+      geolocation.lng = longitude
     }
 
     // store images in firebase
     const storeImage = async (image) => {
       return new Promise((resolve, reject) => {
-        const storage = getStorage();
-        const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
-        const storageRef = ref(storage, 'images/' + fileName);
-        const uploadTask = uploadBytesResumable(storageRef, image);
+        const storage = getStorage()
+        const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+        const storageRef = ref(storage, 'images/' + fileName)
+        const uploadTask = uploadBytesResumable(storageRef, image)
         uploadTask.on(
           'state_changed',
           (snapshot) => {
             const progress =
-              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
             switch (snapshot.state) {
               case 'paused':
-                console.log('Upload is paused');
-                break;
+                console.log('Upload is paused')
+                break
               case 'running':
-                console.log('Upload is running');
-                break;
+                console.log('Upload is running')
+                break
             }
           },
           (error) => {
-            reject(error);
+            reject(error)
           },
           () => {
             getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-              resolve(downloadURL);
-            });
-          }
-        );
-      });
-    };
+              resolve(downloadURL)
+            })
+          },
+        )
+      })
+    }
 
     const imgUrls = await Promise.all(
-      [...images].map((image) => storeImage(image))
+      [...images].map((image) => storeImage(image)),
     ).catch(() => {
-      setLoading(false);
-      toast.error('Images not uploaded');
-      return;
-    });
+      setLoading(false)
+      toast.error('Images not uploaded')
+      return
+    })
 
     const formDataCopy = {
       ...formData,
       imgUrls,
       geolocation,
       timestamp: serverTimestamp(),
-    };
+    }
 
-    delete formDataCopy.images;
-    delete formDataCopy.address;
-    location && (formDataCopy.location = location);
-    !formDataCopy.offer && delete formDataCopy.discountedPrice;
+    delete formDataCopy.images
+    delete formDataCopy.address
+    location && (formDataCopy.location = location)
+    !formDataCopy.offer && delete formDataCopy.discountedPrice
 
-    const docRef = await addDoc(collection(db, 'listings'), formDataCopy);
-    setLoading(false);
-    toast.success('Listing Saved');
-    navigate(`/category/${formDataCopy.type}/${docRef.id}`);
-  };
+    const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
+    setLoading(false)
+    toast.success('Listing Saved')
+    navigate(`/category/${formDataCopy.type}/${docRef.id}`)
+  }
 
   const onMutate = (e) => {
-    let boolean = null;
+    let boolean = null
     if (e.target.value === 'true') {
-      boolean = true;
+      boolean = true
     }
     if (e.target.value === 'false') {
-      boolean = false;
+      boolean = false
     }
 
     // files
@@ -171,7 +175,7 @@ const CreateListing = () => {
       setFormData((prevState) => ({
         ...prevState,
         images: e.target.files,
-      }));
+      }))
     }
 
     // text/booleans/numbers
@@ -179,33 +183,33 @@ const CreateListing = () => {
       setFormData((prevState) => ({
         ...prevState,
         [e.target.id]: boolean ?? e.target.value,
-      }));
+      }))
     }
-  };
+  }
 
-  const auth = getAuth();
+  const auth = getAuth()
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  const isMounted = useRef(true);
+  const isMounted = useRef(true)
 
   useEffect(() => {
     if (isMounted) {
       onAuthStateChanged(auth, (user) => {
         if (user) {
-          setFormData({ ...formData, userRef: user.uid });
+          setFormData({ ...formData, userRef: user.uid })
         } else {
-          navigate('/sign-in');
+          navigate('/sign-in')
         }
-      });
+      })
     }
     return () => {
-      isMounted.current = false;
-    };
-  }, [isMounted]);
+      isMounted.current = false
+    }
+  }, [isMounted])
 
   if (loading) {
-    return <Spinner />;
+    return <Spinner />
   }
 
   return (
@@ -442,6 +446,6 @@ const CreateListing = () => {
         </form>
       </main>
     </div>
-  );
-};
-export default CreateListing;
+  )
+}
+export default CreateListing
